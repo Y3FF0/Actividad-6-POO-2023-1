2563bcdd145741d24db69f379a808c8293aeffd2
Jeffrey Navarro <126307622+Y3FF0@users.noreply.github.com>
2023-06-29 16:28:25 -0500

    Entrega final
    en este proyecto esta la actividad que se pidio en la actividad 6.

---
+++ Actividad_6/src/actividad_6/Actividad_6.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad_6;
+
+/**
+ *
+ * @author yeffo
+ */
+public class Actividad_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Interfaz_grafica formulario= new Interfaz_grafica();
+        formulario.setVisible(true);
+    }
+    
+}
